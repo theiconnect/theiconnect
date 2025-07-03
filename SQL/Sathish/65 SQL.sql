f0f638c9871c5e7bd0c953b311b5b48bb20aeb62
Sathishiconnect25A <sathish.pairala@iconnecttechsolutions.com>
2025-07-03 14:59:20 +0530

    Update 65 SQL.sql

--- SQL/Sathish/65 SQL.sql
+++ SQL/Sathish/65 SQL.sql
@@ -10,12 +10,12 @@
 	SELECT LOC FROM DEPT
 --5.List out first name,last name,salary, commission for all employees
 	SELECT FirstName, LASTNAME, SAL, COMM FROM EMP
---6.List out employee_id,last name,department id for all  employees and rename employee id as ìID  of the employeeî, 
---  last name as ìName of the employeeî, department id as  ìdepartment  IDî
+--6.List out employee_id,last name,department id for all  employees and rename employee id as ‚ÄúID  of the employee‚Äù, 
+--  last name as ‚ÄúName of the employee‚Äù, department id as  ‚Äúdepartment  ID‚Äù
 	SELECT EMPNO AS IDOFEMPLOYEE, lastname AS NAMEOFEMPLOYEE, deptno AS DEPARTMENTID from EMP 
 --7.List out the employees anuual salary with their names only. 
 	SELECT SAL*12 FROM EMP 
---8.List the details about ìSMITHî 
+--8.List the details about ‚ÄúSMITH‚Äù 
 	SELECT * FROM EMP WHERE LastName='SMITH'
 --9.List out the employees who are working in department 20 
 	SELECT * FROM EMP WHERE DEPTNO='20'
@@ -25,11 +25,11 @@
 	SELECT * FROM EMP WHERE DEPTNO BETWEEN 10 AND 20
 --12.Find out the employees who are not working in department 10 or 30 
 	SELECT * FROM EMP WHERE DEPTNO NOT BETWEEN 10 AND 20
---13.List out the employees whose name starts with ìSî 
+--13.List out the employees whose name starts with ‚ÄúS‚Äù 
 	SELECT * FROM EMP WHERE FirstName like 'S%'
---14.List out the employees whose name start with ìSî and end with ìHî
+--14.List out the employees whose name start with ‚ÄúS‚Äù and end with ‚ÄúH‚Äù
 	SELECT * FROM EMP WHERE LastName LIKE 'S%%H'
---15.List out the employees whose name length is 4 and start with ìSî
+--15.List out the employees whose name length is 4 and start with ‚ÄúS‚Äù
 	SELECT * FROM EMP WHERE  LEN(FirstName) = 4 AND FIRSTNAME LIKE 'S%'
 --16.List out the employees who are working in department 10 and draw the salaries more than 3500 
 	SELECT * FROM EMP WHERE DEPTNO = 10 AND SAL >= 3500
@@ -40,8 +40,7 @@
 --19. List out the employee id, name in descending order based on salary column 
 	SELECT EMPNO, FIRSTNAME FROM EMP ORDER BY SAL ASC
 --20. List out the employee details according to their last_name in ascending order and salaries in descending order 
-	SELECT * FROM EMP ORDER BY LastName ASC, 
-	SAL DESC
+	SELECT * FROM EMP ORDER BY LastName ASC, SAL DESC
 --21. List out the employee details according to their last_name in ascending order and then on department_id in descending order.
     SELECT  EMPNO,LASTNAME ,SAL FROM EMP ORDER BY SAL DESC 
 --22.How many employees who are working in different departments wise in the organization For QTP Information
@@ -74,16 +73,18 @@
 	SELECT * FROM EMP WHERE SAL= (SELECT MAX(SAL) FROM EMP)
 -- 35.Display the employees who are working in Sales department
 	SELECT E.* FROM EMP E JOIN DEPT D ON E.DEPTNO = D.DEPTNO WHERE D.DNAME = 'SALES'
--- 36.Display the employees who are working as ìClerkî.
+-- 36.Display the employees who are working as ‚ÄúClerk‚Äù.
 	SELECT * FROM EMP WHERE JOB='CLERK'
--- 37.Display the employees who are working in ìNew Yorkî
+-- 37.Display the employees who are working in ‚ÄúNew York‚Äù
 	SELECT E.* FROM EMP E JOIN DEPT D ON E.DEPTNO=D.DEPTNO WHERE D.LOC='NEW YORK'
--- 38.Find out no.of employees working in ìSalesî department.
+-- 38.Find out no.of employees working in ‚ÄúSales‚Äù department.
 	SELECT COUNT(*) AS NOOFEMPLOYEES FROM EMP E JOIN DEPT D ON E.DEPTNO = D.DEPTNO WHERE D.DNAME = 'SALES'
 -- 39.Update the employees salaries, who are working as Clerk on the basis   of 10%. 
 	UPDATE EMP SET SAL = SAL + (SAL * 0.10) WHERE JOB = 'CLERK'
 -- 40.Delete the employees who are working in accounting department. 
-	
+	update emp set mgr = null where mgr in(
+	SELECT *
+	FROM Emp E JOIN DEPT D ON E.DEPTNO=D.DEPTNO WHERE DNAME ='ACCOUNTING')
 -- 41.Display the second highest salary drawing employee details. 
 	SELECT SAL FROM EMP WHERE SAL=(SELECT MAX(SAL)FROM EMP WHERE SAL<(SELECT MAX(SAL)FROM EMP))
 -- 42.Display the Nth highest salary drawing employee details  
@@ -95,60 +96,54 @@
 -- 45.Find out whose department has not employees. 
 	SELECT D.DEPTNO FROM DEPT D JOIN EMP E ON D.DEPTNO = E.DEPTNO WHERE EMPNO IS NULL
 -- 46.Find out which department does not have any employees.
-	
+	SELECT DNAME,NOOFEMPLOYEES FROM(SELECT DNAME,COUNT(EMPNO)AS NOOFEMPLOYEES
+	FROM DEPT D LEFT JOIN EMP E ON D.DEPTNO = E.DEPTNO GROUP BY DNAME) AS A WHERE NOOFEMPLOYEES=0
 -- 47.Find out the employees who earn greater than the average salary for their department.   
-	
+	SELECT * FROM EMP WHERE SAL>(SELECT AVG(SAL)FROM EMP)
 -- 48.List our employees with their department names 
 	SELECT EMPNO,DNAME FROM EMP E JOIN DEPT D on E.DEPTNO = D.DEPTNO
 -- 49.Display employees with their designations (jobs) 
-	SELECT	
+	SELECT EMPNO, JOB FROM EMP
 -- 50.Display the employees with their department name and regional groups. 
---
+	SELECT E.EMPNO,E.FirstName,E.LastName,D.DEPTNO,D.LOC FROM EMP E JOIN DEPT D ON E.DEPTNO = D.DEPTNO 
 -- 51.How many employees who are working in different departments and display with department name. 
-	SELECT D.DNAME, COUNT(EMPNO)AS NOOFEMPOLYESS FROM EMP E RIGHT JOIN DEPT D ON E.DEPTNO=D.DEPTNO GROUP BY DNAME
+	SELECT D.DNAME, COUNT(EMPNO)AS NOOFEMPOLYESS FROM 
+	EMP E RIGHT JOIN DEPT D ON E.DEPTNO=D.DEPTNO GROUP BY DNAME
 -- 52.How many employees who are working in sales department.
-	SELECT 'SALES' AS DPETNAME,COUNT(EMPNO)AS NOOFEMPLOYEES FROM EMP E JOIN DEPT D ON E.DEPTNO=D.DEPTNO WHERE DNAME='SALES'
+	SELECT 'SALES' AS DPETNAME,COUNT(EMPNO)AS NOOFEMPLOYEES FROM
+	EMP E JOIN DEPT D ON E.DEPTNO=D.DEPTNO WHERE DNAME='SALES'
 -- 53.Which is the department having greater than or equal to 5 employees and display the department names in ascending order. 
 	SELECT D.DEPTNO,D.DNAME FROM EMP E JOIN DEPT D ON E.DEPTNO=D.DEPTNO 
 	GROUP BY D.DEPTNO,DNAME HAVING COUNT(EMPNO)>=5 ORDER BY DNAME	
 -- 54.How many jobs in the organization with designations. 
 	SELECT COUNT(JOB) AS NOOFJOBS,JOB FROM Emp group by JOB
--- 55.How many employees working in ìNew Yorkî.
+-- 55.How many employees working in ‚ÄúNew York‚Äù.
 	SELECT COUNT(*) AS NOOFEMPLOYEES FROM EMP E JOIN DEPT D ON E.DEPTNO=D.DEPTNO WHERE LOC ='New York'
 -- 56.Display employee details with salary grades. 
 	SELECT EMPNO,FirstName,LastName, S.GRADE FROM EMP E JOIN SALGRADE S ON E.SAL BETWEEN S.LOSAL AND S.HISAL
 -- 57.List out the no. of employees on grade wise. 
-	SELECT S.GRADE,COUNT(*)AS NOOFEMPLOYEES FROM EMP E JOIN SALGRADE S on E.SAL BETWEEN S.LOSAL AND S.HISAL GROUP BY GRADE
+	SELECT S.GRADE,COUNT(*)AS NOOFEMPLOYEES FROM 
+	EMP E JOIN SALGRADE S on E.SAL BETWEEN S.LOSAL AND S.HISAL GROUP BY GRADE
 -- 58.Display the employee salary grades and no. of employees between 2000 to 5000 range of salary.
-	SELECT S.GRADE, COUNT(*)AS NOOFEMPLOYEES FROM EMP E JOIN SALGRADE S ON E.SAl BETWEEN S.LOSAL AND S.HISAL WHERE GRADE = 4 GROUP BY GRADE
+	SELECT S.GRADE, COUNT(*)AS NOOFEMPLOYEES FROM 
+	EMP E JOIN SALGRADE S ON E.SAl BETWEEN S.LOSAL AND S.HISAL WHERE GRADE = 4 GROUP BY GRADE
 -- 59.Display the employee details with their manager names.
 	SELECT E.EmpNo,E.FirstName,E.LastName,M.MGR FROM EMP E JOIN EMP M ON E.EMPNO = M.EMPNO
 -- 60.Display the employee details who earn more than their managers salaries.
-	SELECT E.EMPNO,E.FirstName,E.LastName,E.SAL AS EMPSAL,M.SAL AS MGRSAL FROM EMP E JOIN EMP M ON E.MGR = M.MGR WHERE E.SAL>M.SAL
---61.Show the no. of employees working under every manager.
-
---61.Display employee details with all departments.
-	SELECT *
-	FROM Emp E
-	Right Join Dept D on E. DeptNo = D. DeptNo
-
---62.Display all employees in sales or operation departments.
-	SELECT EmpNo,FirstName,LastName,Sal,Dname
-	FROM Emp E
-	left join Dept D on E. DeptNo = D. DeptNo
+	SELECT E.EMPNO,E.FirstName,E.LastName,E.SAL AS EMPSAL,M.SAL AS MGRSAL FROM 
+	EMP E JOIN EMP M ON E.MGR = M.MGR WHERE E.SAL>M.SAL
+-- 61.Show the no. of employees working under every manager.
+	SELECT * FROM EMP E RIGHT JOIN DEPT D ON E.DEPTNO=D.DEPTNO
+-- 61.Display employee details with all departments.
+	SELECT * FROM Emp E Right Join Dept D on E. DeptNo = D. DeptNo
+-- 62.Display all employees in sales or operation departments.
+	SELECT EmpNo,FirstName,LastName,Sal,Dname FROM Emp E left join Dept D on E. DeptNo = D. DeptNo
 	WHERE DName = 'SALES' or DName = 'operations'
-
 --63.List out the distinct jobs in Sales and Accounting Departments.
-	SELECT DISTINCT Job,Dname
-	FROM Emp E
-    left join Dept D on E. DeptNo = D. DeptNo
+	SELECT DISTINCT Job,Dname FROM Emp E left join Dept D on E. DeptNo = D. DeptNo 
 	WHERE DName = 'SALES' or DName = 'ACCOUNTING'
-	
 --64.List out the ALL jobs in Sales and Accounting Departments.
-	SELECT Job,DName
-	FROM Emp E
-	left join Dept D on E. DeptNo = D. DeptNo
-	WHERE DName = 'SALES' or DName = 'ACCOUNTING'
-
+	SELECT Job,DName FROM Emp E left join Dept D on E. DeptNo = D. DeptNo WHERE DName = 'SALES' or DName = 'ACCOUNTING'
 --65.List out the common jobs in Research and Accounting Departments in ascending order.
-	SELECT JOB,DNAME FROM EMP E INNER JOIN DEPT D ON E.DEPTNO= D.DEPTNO WHERE DNAME='RESEARCH' OR DNAME='ACCOUNTING'
+	SELECT JOB,DNAME FROM EMP E INNER JOIN DEPT D ON E.DEPTNO= D.DEPTNO 
+	WHERE DNAME='RESEARCH' OR DNAME='ACCOUNTING'
