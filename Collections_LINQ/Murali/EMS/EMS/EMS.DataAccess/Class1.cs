5787ae5e2d2828e24a604f7b15e10936a5f25d91
Murali <theiconnect.org@gmail.com>
2025-11-15 19:41:40 +0530

    projects template creation and references are added.

---
+++ Collections_LINQ/Murali/EMS/EMS/EMS.DataAccess/Class1.cs
@@ -0,0 +1,19 @@
+﻿using ns1;
+using EMS.Models;
+namespace EMS.DataAccess
+{
+    public class Class1
+    {
+        class2 obj = new class2();
+        class3 obj2 = new ns1.class3();
+        ClassModel obj3 = new ClassModel();
+    }
+
+    public class class2 { }
+
+}
+
+namespace ns1
+{
+    public class class3 { }
+}
