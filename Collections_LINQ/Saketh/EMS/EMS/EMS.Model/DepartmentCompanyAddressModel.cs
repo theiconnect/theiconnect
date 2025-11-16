f3a7b9de906ba8ffc35bff5b170adef053ffc182
Gangishetty Saketh Sai <Sakethsai.gangishetty@iconnecttechsolutions.com>
2025-11-16 21:10:06 +0530

    created all class  models in EMS.Models

---
+++ Collections_LINQ/Saketh/EMS/EMS/EMS.Model/DepartmentCompanyAddressModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS.Model
+{
+    public class DepartmentCompanyAddressModel
+    {
+      public int  DepartmentCompanyAddressXREFIDPk {  get; set; }
+      public DepartmentModel  DepartmentIdFk {  get; set; }                      
+      public CompanyAddressModel CompanyAddressIdFk {  get; set; }             
+
+    }
+}
---
+++ Collections_LINQ/Saketh/EMS/EMS/EMS.Model/DepartmentModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS.Model
+{
+    public class DepartmentModel
+    {
+public  int DepartmentIdPk {  get; set; }
+public  string DepartmentCode {  get; set; }
+public   string DepartmentName {  get; set; }
+public string Location {  get; set; }
+public Boolean IsActive {  get; set; }
+
+        List<DepartmentModel> Departments { get; set; } = new List<DepartmentModel>();  
+
+    }
+}
