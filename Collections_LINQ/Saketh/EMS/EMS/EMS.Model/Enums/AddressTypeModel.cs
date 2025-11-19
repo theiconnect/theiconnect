afa2b264824b9307d674bcdf5b1bccd0b105bb59
Gangishetty Saketh Sai <Sakethsai.gangishetty@iconnecttechsolutions.com>
2025-11-19 19:10:12 +0530

    created all cls models and with data in it

---
+++ Collections_LINQ/Saketh/EMS/EMS/EMS.Model/Enums/AddressTypeModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS.Model.Enums
+{
+        public enum CompanyAddressTypes
+        {
+            Corporate = 1,
+            Otheroffice = 2
+        }
+
+        public enum EmployeeAddressTypes
+        {
+            Permanent = 0,
+            Present = 1
+        }
+
+    
+}
