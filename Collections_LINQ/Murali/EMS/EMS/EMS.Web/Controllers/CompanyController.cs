e1f45442eeb2ea65c00e2fd6c9734f865e0cfde1
ThrishaIconnect-25A <thrishasri.masetty@iconnecttechsolutions.com>
2025-12-10 13:10:21 +0530

    I have created the company controllers and created the view controller page.

---
+++ Collections_LINQ/Murali/EMS/EMS/EMS.Web/Controllers/CompanyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EMS.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EMS.Web.Controllers
+{
+    [Route("Company")]
+    public class CompanyController : Controller
+    {
+        [Route("edit")]
+        public IActionResult EditCompany() => View();
+
+        [Route("list")]
+        public IActionResult CompanyList()
+        {
+            // Replace with real data retrieval (repository/service) as needed.
+            IEnumerable<CompanyModel> companies = Enumerable.Empty<CompanyModel>();
+            return View(companies);
+        }
+    }
+}
--- Collections_LINQ/Murali/EMS/EMS/EMS.Web/Program.cs
+++ Collections_LINQ/Murali/EMS/EMS/EMS.Web/Program.cs
@@ -40,7 +40,7 @@ app.UseAuthorization();
 //Conventional Routing
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=company}/{action=list}/{id?}");
 
 //app.MapControllerRoute(
 //    name: "default1",
