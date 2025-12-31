2470de361ce4cd37769441d73cd32f49f8ce9834
gouthamilegala <gouthamilegala12345@gmail.com>
2025-12-31 12:43:06 +0530

    i have changed some code statement

--- Collections_LINQ/Murali/EMS/EMS/EMS.Web/Controllers/CompanyController.cs
+++ Collections_LINQ/Murali/EMS/EMS/EMS.Web/Controllers/CompanyController.cs
@@ -2,12 +2,21 @@
 using System.Linq;
 using EMS.Models;
 using Microsoft.AspNetCore.Mvc;
+using EMS.IServices;
 
 namespace EMS.Web.Controllers
 {
-    
+
+
+    [Route("Company")]
     public class CompanyController : Controller
     {
+
+        private ICompanyService companyservice;
+        public CompanyController(ICompanyService _obj)
+        {
+            companyservice = _obj;
+        }
         //[Route("edit")]
         public IActionResult EditCompany()
         {
@@ -19,8 +28,29 @@ namespace EMS.Web.Controllers
         //[Route("view")]
         //[Route("details")]
        public  IActionResult ViewCompany() 
+        
         {
-            return View();
+            var companyDB = companyservice.GetCompany();
+
+            //var companymodel = new CompanyModel(){
+            //    companymodel.CompanyName = companyDB.CompanyName,
+            //    companymodel.PhoneNumber = companyDB.PhoneNumber,
+            //    companymodel.BankAccountNumber = companyDB.BankAccountNumber,
+
+            //};
+
+            var companymodel = new CompanyModel();
+            companymodel.CompanyIdPk = companyDB.CompanyIdPk;
+            companymodel.CompanyName = companyDB.CompanyName;
+            companymodel.PhoneNumber = companyDB.PhoneNumber;
+            companymodel.TIN = companyDB.TIN;
+            companymodel.BankAccountNumber = companyDB.BankAccountNumber;
+            companymodel.RegistrationDate = companyDB.RegistrationDate;
+            companymodel.PAN = companyDB.PAN;
+            companymodel.Website = companyDB.Website;
+            companymodel.Email = companyDB.Email;
+
+            return View(companymodel);
         }
 
         //[Route("list")]
---
+++ Collections_LINQ/Murali/EMS/EMS/EMS.Web/Models/CompanyViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace EMS.Web.Models
+{
+    public class CompanyViewModel
+    {
+        public int CompanyIdPk { get; set; }
+        public string CompanyName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public string Website { get; set; }
+        public string BankAccountNumber { get; set; }
+        public string TIN { get; set; }
+        public string PAN { get; set; }
+    }
+}
