01e0a6b60f44816dbe1de7136676d7f374f5ce41
ThrishaIconnect-25A <thrishasri.masetty@iconnecttechsolutions.com>
2026-01-07 11:11:22 +0530

    Changed the comapny model and added the company data.

--- Collections_LINQ/Murali/EMS/EMS/EMS.Models/CompanyModel.cs
+++ Collections_LINQ/Murali/EMS/EMS/EMS.Models/CompanyModel.cs
@@ -8,7 +8,7 @@
         public string Email { get; set; }
         public DateTime? RegistrationDate { get; set; }
         public string Website { get; set; }
-        public string BankAccountNumber { get; set; }
+       
         public string TIN { get; set; }
         public string PAN { get; set; }
 
