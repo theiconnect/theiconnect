d63eb8891b0deb0b6a121d089a7fe657789a25c9
ThrishaIconnect-25A <thrishasri.masetty@iconnecttechsolutions.com>
2026-01-07 19:09:37 +0530

    Created the  ComapnyADO setrvice page and added the comapny controller

---
+++ Collections_LINQ/Murali/EMS/EMS/EMS.Services/Implementation/ADO/CompanyADOService.cs
@@ -0,0 +1,18 @@
+﻿using EMS.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMS.Services.Implementation.ADO
+{
+    public class CompanyADOService
+    {
+        public CompanyADOService() 
+        { 
+            
+
+        }
+    }
+}
